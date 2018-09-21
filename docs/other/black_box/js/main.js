9db743cae1f13b94d40357e3c0fcd5dfd6c35b3a
Sutaa0101 <sutaahbts@yahoo.co.jp>
2018-09-22 04:44:24 +0900

    **************

---
+++ docs/other/black_box/js/main.js
@@ -0,0 +1,24 @@
+document.onkeydown = function(event){
+    let keyCode = event.code;
+    let target = event.target;
+    if(keyCode == "Enter" && target.type == "text"){
+        let submit = document.getElementById("searchSubmit");
+        return submit.onclick();
+    }    
+}
+
+function inputSearch(){
+    let inputCode = document.getElementById("inputCode");
+    createOutput(inputCode.value);
+    inputCode.value = "";
+}
+
+function createOutput(code){
+    let inputCode = document.createTextNode(code);
+    let outputArea = document.getElementById("outputArea");
+    let newDiv = document.createElement("div");
+
+    newDiv.setAttribute("class","sentence");
+    newDiv.appendChild(inputCode);
+    outputArea.appendChild(newDiv);
+}
